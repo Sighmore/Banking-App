7cd1467ee50dbb79d9dfb652ddbdea9bc0913c1a
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-04-23 18:15:47 +0500

    Icons are added
    Signed-off-by: SanjarIsaboev <sanjarisaboev@yandex.com>

--- app/src/main/java/sanjar/uz/bankingappui/MainActivity.kt
+++ app/src/main/java/sanjar/uz/bankingappui/MainActivity.kt
@@ -3,13 +3,17 @@ package sanjar.uz.bankingappui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import sanjar.uz.bankingappui.ui.theme.BankingAppUITheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +34,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen() {
+    Scaffold(bottomBar = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankingAppUITheme {
-        Greeting("Android")
+    }) { padding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(padding)) {
+            //WalletSection()
+            //CardsSection()
+            Spacer(modifier = Modifier.height(16.dp))
+            //FinanceSection()
+            //CurrentsiesSection()
+        }
+
     }
 }
+
